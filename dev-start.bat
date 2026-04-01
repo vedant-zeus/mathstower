31e1d555f9a5107048324737b3cd4255409ca380
Vedant  Amrutkar <vedant8405@gmail.com>
2026-04-01 13:18:48 +0530

    Working

--- dev-start.bat
+++ dev-start.bat
@@ -2,7 +2,7 @@
 echo Starting Trigno Tower (Backend & Frontend)...
 
 start cmd /k "cd backend && npm run dev"
-start cmd /k "cd frontend && npm run dev -- --hostname 0.0.0.0"
+start cmd /k "cd frontend && npm run dev -- --hostname 10.9.125.95"
 
 echo Servers are initializing in separate windows.
 echo - Frontend: http://localhost:3000 (and your LAN IP)
